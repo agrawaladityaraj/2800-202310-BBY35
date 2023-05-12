fc642ba73384116402925df72694d4e24c2d5252
shawn birring <sbirring4@my.bcit.ca>
2023-05-11 17:26:18 -0700

    added a page for dog breeds and the according api call to get them

---
+++ pages/api/dog-breeds.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  try {
+    const dogBreeds = await prisma.dogBreed.findMany();
+    res.status(200).json(dogBreeds);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch dog breeds" });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
---
+++ pages/dogbreeds.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+export default function DogBreeds() {
+  const [dogBreeds, setDogBreeds] = useState([]);
+
+  useEffect(() => {
+    fetchDogBreeds();
+  }, []);
+
+  const fetchDogBreeds = async () => {
+    const response = await fetch("/api/dogbreeds");
+    const data = await response.json();
+    setDogBreeds(data);
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "breed", headerName: "Breed", width: 130 },
+    { field: "countryOfOrigin", headerName: "Country of Origin", width: 130 },
+    { field: "furColor", headerName: "Fur Color", width: 130 },
+    { field: "heightIn", headerName: "Height (in)", width: 130 },
+    { field: "colorOfEyes", headerName: "Color of Eyes", width: 130 },
+    { field: "longevityYrs", headerName: "Longevity (yrs)", width: 130 },
+    { field: "characterTraits", headerName: "Character Traits", width: 200 },
+    {
+      field: "commonHealthProblems",
+      headerName: "Common Health Problems",
+      width: 200,
+    },
+  ];
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={dogBreeds}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+      />
+    </div>
+  );
+}
